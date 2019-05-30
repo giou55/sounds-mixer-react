90f0587e27e32a00dbd70d291f61d0770a7c0524
Giorgosg <giourmetakis00@gmail.com>
2019-05-30 14:09:52 +0300

    changes

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -4,6 +4,7 @@ import './Header.css';
 const Header = (props) => {
     return (
         <div className="header">
+          <h1>Calm Down and Listen</h1>
           <button className="btn" 
                   onClick={props.playAllSounds}>{props.playAllStatus ? 'PAUSE' : 'PLAY'}
             <i className={props.playAllStatus ? 'fas fa-pause' : 'fas fa-play'}></i>
